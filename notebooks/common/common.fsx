8bfdb18437528cc2dfabbabdcd5ae28bffa5de61
mazharenko <mazharenko.a@yandex.ru>
2022-12-02 14:19:46 +0500

    day 2

--- notebooks/common/common.fsx
+++ notebooks/common/common.fsx
@@ -5,6 +5,10 @@ let displayPipe x =
     x |> display |> ignore
     x
 
+let splitToTuple2 (separators : string array) (s : string) =
+    let split = s.Split(separators, System.StringSplitOptions.RemoveEmptyEntries)
+    split.[0], split.[1]
+    
 module Pattern1 =
     let read (f : string -> 'a) (data : string) = 
         data.Split([|"\n"; "\r"|], System.StringSplitOptions.RemoveEmptyEntries) 
