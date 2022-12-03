0955453b92163386c3068c9ef9793891fb31887f
mazharenko <mazharenko.a@yandex.ru>
2022-12-03 16:13:26 +0500

    day 3

--- notebooks/common/common.fsx
+++ notebooks/common/common.fsx
@@ -8,6 +8,9 @@ let displayPipe x =
 let splitToTuple2 (separators : string array) (s : string) =
     let split = s.Split(separators, System.StringSplitOptions.RemoveEmptyEntries)
     split.[0], split.[1]
+
+let (|BetweenInclusive|_|) min max x =
+    if min <= x && x <= max then Some () else None
     
 module Pattern1 =
     let read (f : string -> 'a) (data : string) = 
