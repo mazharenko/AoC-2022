83dbf2fddd6993fb545bafcea7e8c70522c00021
mazharenko <mazharenko.a@yandex.ru>
2022-12-11 00:04:56 +0500

    embed js script to (hopefully) fix nbviewer

--- notebooks/common/matrixFormatting.fsx
+++ notebooks/common/matrixFormatting.fsx
@@ -32,16 +32,36 @@ let private template =
             }
         </script>
         <script>
-            if (typeof DrawMatrix !== 'object') {
-                var script = document.createElement("script");
-                script.setAttribute("src", "[DIR]/matrixFormatting.js");
-                script.onload = function() {
-                    render_[ID]();
-                };
-                document.getElementsByTagName("head")[0].appendChild(script);
-            }
-            else
-                render_[ID]();
+            (function () {
+                if (typeof DrawMatrix === 'object') {
+                    return;
+                }
+
+                DrawMatrix = {
+                    render: function(containerId, data, settings) {
+                        let container = document.getElementById(containerId);
+                        container.innerHTML = "";
+                        let canvas = document.createElement("canvas");
+                        container.appendChild(canvas);
+
+                        const size = settings.size;
+                        canvas.width = settings.width;
+                        canvas.height = settings.height;
+
+                        var ctx = canvas.getContext("2d");
+                        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                        for (point of data) {
+                            const [i, j, color] = point;
+                            ctx.fillStyle = color;
+                            ctx.fillRect(size * j, size * i, size, size);
+                        }
+                    }
+                }
+            })();
+        </script>
+        <script>
+            render_[ID]();
         </script>
 
     """
