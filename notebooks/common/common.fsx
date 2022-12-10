fad467628b92a7b45bd9ad0f50e4015dc0bbf817
mazharenko <mazharenko.a@yandex.ru>
2022-12-10 23:13:51 +0500

    day 10

--- notebooks/common/common.fsx
+++ notebooks/common/common.fsx
@@ -11,6 +11,13 @@ let splitToTuple2 (separators : string array) (s : string) =
 
 let (|BetweenInclusive|_|) min max x =
     if min <= x && x <= max then Some () else None
+
+let (|Regex|_|) pattern input =
+    let m = System.Text.RegularExpressions.Regex.Match(input, pattern)
+    if m.Success then
+        Some(List.tail [ for g in m.Groups -> g.Value ])
+    else
+        None
     
 module Pattern1 =
     let read (f : string -> 'a) (data : string) = 
